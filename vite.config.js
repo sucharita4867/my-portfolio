e305bb4e52810082b071d148e2723fb672743a6d
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-05 13:08:59 +0530

    create my protfolio

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
