eabaddbbacac363cb43dfd0e66764869a66fad04
sucharita4867 <sucharitasardar7@gmail.com>
2026-02-15 16:52:01 +0530

    update projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,6 @@ const Projects = () => {
     >
       <div className="content-container">
         <motion.div ref={ref}>
-          {/* Section Header */}
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
