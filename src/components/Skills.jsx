d46e2d756459bc60892d7ec80034c2b2b86b99ea
sucharita4867 <sucharitasardar7@gmail.com>
2026-02-16 22:54:58 +0530

    delete line

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -103,7 +103,6 @@ const Skills = () => {
             />
           </div>
 
-          {/* Additional Info */}
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
